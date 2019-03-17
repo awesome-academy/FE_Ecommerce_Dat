0a53c28ef1ea69f610bc418e0235ff808cd7042a
Nguyen Manh Dat <nguyen.manh.dat@framgia.com>
2019-03-17 14:01:41 +0700

    Project Configuration

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './containers/Home'
+class App extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        )
+    }
+}
+export default App;
