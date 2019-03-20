587df3406986c0309d5e7961532acb03a40d5694
Nguyen Manh Dat <nguyen.manh.dat@framgia.com>
2019-03-20 09:38:41 +0700

    Re-config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,87 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// Try the environment variable, otherwise use root
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: "style-loader"
-      },
-      {
-        test: /\.(css|scss)$/,
-        loader: "css-loader",
-        query: {
-          modules: true,
-          localIdentName: "[name]__[local]___[hash:base64:5]"
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.scss', '.css', '.sass']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  }
+	mode: 'development',
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: [/\.scss$/, /\.sass$/],
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'resolve-url-loader',
+						options: {
+							debug: true,
+							root: '',
+							includeRoot: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							outputStyle: 'compressed',
+							sourceMap: true,
+							includePaths: [
+								'./src'
+							],
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx', '.scss', '.css', '.sass']
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new webpack.DefinePlugin({
+			'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		})
+	],
+	devServer: {
+		contentBase: "dist",
+		hot: true
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+		publicPath: ASSET_PATH
+	},
 }
